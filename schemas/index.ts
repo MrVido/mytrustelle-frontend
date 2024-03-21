8317a2756981a7f3e11d2b6acde25b91f6a03f4b
mrvido <zvonividovic@gmail.com>
2024-03-20 22:19:22 -0400

    register-form created

--- schemas/index.ts
+++ schemas/index.ts
@@ -9,3 +9,16 @@ export const LoginSchema = z.object({
 
     }),
 });
+
+export const RegisterSchema = z.object({
+    email: z.string().email({
+        message: "Is you Dumb? Email"
+    }),
+    password: z.string().min(6, {
+        message: "Minimum 6 characters required for registration"
+
+    }),
+    name: z.string().min(3, {
+        message: "Please Enter your name"
+    })
+});
