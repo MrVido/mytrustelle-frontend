15475ac23e1463d0e4f7cb032b66ce8dccc548a7
mrvido <zvonividovic@gmail.com>
2024-03-21 00:22:31 -0400

    password reset fixed.

--- actions/register.ts
+++ actions/register.ts
@@ -1,38 +1,34 @@
 "use server"
 
 import * as z from "zod";
-import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    const validatedFields = RegisterSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
 
-    if (!validatedFields.success) {
-        return { error: "Invalid fields!" };
-    }
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    const {email, password, name} = validatedFields.data;
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    const existingUser = await db.user.findUnique({
-        where: { email,
-        }
-    });
-    if (existingUser) {
-        return { error: "Email already in use!"};
-    }
+  const existingUser = await getUserByEmail(email);
 
-    await db.user.create({
-        data: {
-            name,
-            email,
-            password: hashedPassword,
-        },
-    });
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
 
-
-
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 
     return { success: "User Created!" };
 };
