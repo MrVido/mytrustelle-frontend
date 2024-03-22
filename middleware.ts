fc7aee130241eb5413d21ec4520b812ae09f82b0
mrvido <zvonividovic@gmail.com>
2024-03-22 11:20:50 -0400

    still the middleware bug

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,8 @@ const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   const { nextUrl } = req;
+
+
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
