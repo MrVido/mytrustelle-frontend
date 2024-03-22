916eda64434f302e03bbf26f562679de063e6565
mrvido <zvonividovic@gmail.com>
2024-03-22 00:55:26 -0400

    adjustments

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,10 @@
+import { auth } from "@/auth"
+
+const Settingspage = async () => {
+    const session = await auth();
+  return (
+    <div>{JSON.stringify(session)}</div>
+  )
+}
+
+export default Settingspage
