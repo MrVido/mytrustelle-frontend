4067545929e4ad9c23a226fb7885583932e24286
mrvido <zvonividovic@gmail.com>
2024-03-22 00:12:47 -0400

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { auth } from "@/auth";
+
+export default auth((req) => {
+    const isLoggedIn = !!req.auth;
+    
+})
+
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
