f13767dc29c22299df347fad779c5a49246b17b6
mrvido <zvonividovic@gmail.com>
2024-03-22 00:29:25 -0400

    middleware next auth

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
-import { auth } from "@/auth";
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+
+
+const { auth } = NextAuth(authConfig);
+
 
 export default auth((req) => {
     const isLoggedIn = !!req.auth;
-    
+    console.log("Route: ", req.nextUrl.pathname)
+    console.log("IsLoggedIn: ", isLoggedIn);
+
 })
 
 
